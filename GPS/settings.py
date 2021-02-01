38ae2ae333884782a2feb3938ed04a9a0858b8de
Naim Perves <naimperves78@gmail.com>
2021-02-01 20:40:35 +0600

    second Commit

--- GPS/settings.py
+++ GPS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9!zhe_&zsw$@$_vkx-&g8s!c($lkg-d%(es)61$%1^p45s^gf#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["TahrimaMoon.pythonanywhere.com"]
 
 
 # Application definition
